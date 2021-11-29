5c9e3efb28c68504001de480490dd386738d8e5f
Gustavo Cruz <gu.cruz17@hotmail.com>
2021-11-29 11:41:56 -0300

    Implementando o getBookByIsbn na camada de serviço

--- src/main/java/com/gustavo/libraryapi/service/impl/BookServiceImpl.java
+++ src/main/java/com/gustavo/libraryapi/service/impl/BookServiceImpl.java
@@ -71,7 +71,7 @@ public class BookServiceImpl implements BookService {
 
 	@Override
 	public Optional<Book> getBookByIsbn(String isbn) {
-		return null;
+		return repository.findByIsbn(isbn);
 	}
 
 }
