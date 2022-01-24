2e4aca70c4a4018e8e4d7d03a541fa94cf993957
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-01-23 22:17:35 -0300

    Adicionando comentários sobre Query by Example

--- src/main/java/com/gustavo/libraryapi/service/impl/BookServiceImpl.java
+++ src/main/java/com/gustavo/libraryapi/service/impl/BookServiceImpl.java
@@ -55,12 +55,18 @@ public class BookServiceImpl implements BookService {
 	public Page<Book> find(Book filter, Pageable pageRequest) {
 		
 		// Query by Example
+		// A consulta por exemplo (QBE) é uma técnica de consulta amigável com uma interface simples. Ele permite a criação de 
+		// consultas dinâmicas e não exige que você escreva consultas que contenham nomes de campos e nem que você escreva 
+		// consultas usando linguagens de consulta específicas.
+		
 		Example<Book> example = Example.of(filter, 
 				ExampleMatcher
-					.matching()
-					.withIgnoreCase()
-					.withIgnoreNullValues()
-					.withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING));
+					.matching() // Permite que as configurações sejam feitas
+					.withIgnoreCase()// Nos campos string, vai verificar no banco ignorando se o usuário passou valor em cauxa alta 
+									//	ou em caixa baixa
+					.withIgnoreNullValues()// Se foi passado alguma propriedade nula, será ignorada
+					.withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING));// Vai informar se comparação das string serão 
+												// feitas pelo início, pelo fim, em qualquer parte ou o valor exato que foi passado
 		
 		return repository.findAll(example, pageRequest);
 	}
